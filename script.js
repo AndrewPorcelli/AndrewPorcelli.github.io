73ad08540117fe21a058a079bf1b1f92d8cd9a89
Andrew Porcelli <andy.porcelli1@gmail.com>
2022-07-15 11:25:44 -0600

    changed text

--- script.js
+++ script.js
@@ -90,17 +90,4 @@ document.getElementById('venmo').addEventListener('click', () => {
 let link = 'https://account.venmo.com/u/Hyrum-Sorensen'
 location.href = link;
 })
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
+
