f3400ac781d6453a406bdd4d68792009ee7344b9
Andrew Porcelli <andy.porcelli1@gmail.com>
2022-07-18 20:32:35 -0600

    added links to projects and github

--- script.js
+++ script.js
@@ -1,93 +1,94 @@
- //javascript for navigation bar effects on scroll
- window.addEventListener("scroll", function(){
-    const header = document.querySelector("header");
-    header.classList.toggle('sticky', window.scrollY > 0);
+//javascript for navigation bar effects on scroll
+window.addEventListener('scroll', function () {
+  const header = document.querySelector('header');
+  header.classList.toggle('sticky', window.scrollY > 0);
+});
+
+//javascript for responsive navigation sidebar menu
+const menuBtn = document.querySelector('.menu-btn');
+const navigation = document.querySelector('.navigation');
+const navigationItems = document.querySelectorAll('.navigation a');
+
+menuBtn.addEventListener('click', () => {
+  menuBtn.classList.toggle('active');
+  navigation.classList.toggle('active');
+});
+
+navigationItems.forEach((navigationItem) => {
+  navigationItem.addEventListener('click', () => {
+    menuBtn.classList.remove('active');
+    navigation.classList.remove('active');
   });
-  
-  //javascript for responsive navigation sidebar menu
-  const menuBtn = document.querySelector(".menu-btn");
-  const navigation = document.querySelector(".navigation");
-  const navigationItems = document.querySelectorAll(".navigation a")
-  
-  menuBtn.addEventListener("click",  () => {
-    menuBtn.classList.toggle("active");
-    navigation.classList.toggle("active");
-  });
-  
-  navigationItems.forEach((navigationItem) => {
-    navigationItem.addEventListener("click", () => {
-      menuBtn.classList.remove("active");
-      navigation.classList.remove("active");
-    });
-  });
-  
-  //javascript for scroll to top button
-  const scrollBtn = document.querySelector(".scrollToTop-btn");
-  
-  window.addEventListener("scroll", function(){
-    scrollBtn.classList.toggle("active", window.scrollY > 500);
-  });
-  
-  //javascript for scroll back to top on click scroll-to-top button
-  scrollBtn.addEventListener("click", () => {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-  });
-  
-  //javascript for reveal website elements on scroll
-  window.addEventListener("scroll", reveal);
-  
-  function reveal(){
-    var reveals = document.querySelectorAll(".reveal");
-  
-    for(var i = 0; i < reveals.length; i++){
-      var windowHeight = window.innerHeight;
-      var revealTop = reveals[i].getBoundingClientRect().top;
-      var revealPoint = 50;
-  
-      if(revealTop < windowHeight - revealPoint){
-        reveals[i].classList.add("active");
-      }
+});
+
+//javascript for scroll to top button
+const scrollBtn = document.querySelector('.scrollToTop-btn');
+
+window.addEventListener('scroll', function () {
+  scrollBtn.classList.toggle('active', window.scrollY > 500);
+});
+
+//javascript for scroll back to top on click scroll-to-top button
+scrollBtn.addEventListener('click', () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
+
+//javascript for reveal website elements on scroll
+window.addEventListener('scroll', reveal);
+
+function reveal() {
+  var reveals = document.querySelectorAll('.reveal');
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var revealTop = reveals[i].getBoundingClientRect().top;
+    var revealPoint = 50;
+
+    if (revealTop < windowHeight - revealPoint) {
+      reveals[i].classList.add('active');
     }
   }
-        
+}
+
 document.getElementById('project-1').addEventListener('click', () => {
-location.href = 'https://mewsika.herokuapp.com';
-})
+  location.href =
+    'https://www.youtube.com/watch?v=5u6JjHNL2U4&ab_channel=AndrewPorcelli';
+});
 document.getElementById('project-2').addEventListener('click', () => {
-location.href = 'https://github.com/HyrumSorensen/CS236-Datalog-Program';
-})
+  location.href = '';
+});
 document.getElementById('email-icon').addEventListener('click', () => {
-let link = 'mailto:hgsoren@byu.edu'
-location.href = link;
-})
+  let link = 'andrewjporcelli@gmail.com';
+  location.href = link;
+});
 document.getElementById('website-icon').addEventListener('click', () => {
-let link = 'https://hyrumsorensen.github.io'
-location.href = link;
-})
+  let link = 'https://andrewporcelli.github.io';
+  location.href = link;
+});
 
 document.getElementById('address-icon').addEventListener('click', () => {
-let link = 'https://www.google.com/maps/place/Provo,+UT/@40.2572423,-111.9260001,11z/data=!3m1!4b1!4m5!3m4!1s0x874d9271930bf1bf:0x1d90f12600b556ef!8m2!3d40.2338438!4d-111.6585337'
-location.href = link;
-})
+  let link =
+    'https://www.google.com/maps/place/Provo,+UT/@40.2572423,-111.9260001,11z/data=!3m1!4b1!4m5!3m4!1s0x874d9271930bf1bf:0x1d90f12600b556ef!8m2!3d40.2338438!4d-111.6585337';
+  location.href = link;
+});
 document.getElementById('insta').addEventListener('click', () => {
-let link = 'https://www.instagram.com/hyrum_sorensen/'
-location.href = link;
-})
+  let link = '';
+  location.href = link;
+});
 document.getElementById('git-hub').addEventListener('click', () => {
-let link = 'https://github.com/HyrumSorensen'
-location.href = link;
-})
+  let link = 'https://github.com/AndrewPorcelli';
+  location.href = link;
+});
 document.getElementById('discord').addEventListener('click', () => {
-let link = 'https://discordapp.com/users/9609/'
-location.href = link;
-})
+  let link = 'https://discordapp.com/users/';
+  location.href = link;
+});
 document.getElementById('facebook').addEventListener('click', () => {
-let link = 'https://www.facebook.com/hyrumgary/'
-location.href = link;
-})
+  let link = 'https://www.facebook.com/';
+  location.href = link;
+});
 document.getElementById('venmo').addEventListener('click', () => {
-let link = 'https://account.venmo.com/u/Hyrum-Sorensen'
-location.href = link;
-})
-
+  let link = 'https://account.venmo.com/u/';
+  location.href = link;
+});
